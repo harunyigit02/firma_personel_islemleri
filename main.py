2a36a47e554b99f8d0d9a27c76dac52aefd93ee8
harunyigit02 <156890324+harunyigit02@users.noreply.github.com>
2024-03-27 11:28:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-class Personel:
-
-    def __init__(self, adi, departman, yil, maas):
-        self.adi = adi
-        self.departman = departman
-        self.yil = yil
-        self.maas = maas
-
-
-class Firma:
-    personellistesi = []
-
-
-    def personel_ekle(self, personel):
-
-        self.personellistesi.append(personel)
-
-    def personel_listele(self):
-        for personel in self.personellistesi:
-            print("ad:",personel.adi,"departman:",personel.departman,"yil:",personel.yil,"maas:",personel.maas)
-
-    def maas_zammi(self,personel,maas_oranı):
-        personel.maas*=maas_oranı
-
-
-    def personel_cikart(self,personel):
-        self.personellistesi.remove(personel)
-
-ödev3
+from firma import Personel
+from firma import Firma
+a=Personel("harun","muhendis",211,20000)
+b=Personel("asd","sdfsdf",222,500000)
+firma=Firma()
+firma.personel_ekle(a)
+firma.personel_ekle(b)
+firma.personel_listele()
+firma.maas_zammi(a,2)
+firma.personel_cikart(b)
+firma.personel_listele()
