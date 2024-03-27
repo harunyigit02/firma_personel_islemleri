c0ef322caa1daee60cb5c99b0b14f3e233075e60
harunyigit02 <156890324+harunyigit02@users.noreply.github.com>
2024-03-27 11:25:22 +0300

    Add files via upload

---
+++ firma.py
@@ -0,0 +1,25 @@
+class Personel:
+
+    def __init__(self, adi, departman, yil, maas):
+        self.adi = adi
+        self.departman = departman
+        self.yil = yil
+        self.maas = maas
+
+
+class Firma:
+    personellistesi = []
+
+    def personel_ekle(self, personel):
+        self.personellistesi.append(personel)
+
+    def personel_listele(self):
+        for personel in self.personellistesi:
+            print("ad:", personel.adi, "departman:", personel.departman, "yil:", personel.yil, "maas:", personel.maas)
+
+    def maas_zammi(self, personel, maas_oranı):
+        personel.maas *= maas_oranı
+
+    def personel_cikart(self, personel):
+        self.personellistesi.remove(personel)
+
